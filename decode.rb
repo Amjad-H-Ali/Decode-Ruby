da5494a30226ad01b51e82b4354f5e8d2f9a504b
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-09-23 22:10:24 -0500

    Fixed bug regarding slice(start, length) method

--- decode.rb
+++ decode.rb
@@ -2,6 +2,7 @@ p "Decode!"
 
 # Function takes in string of digits and 
 # k representing current portion of string being evaluated
+# Returns number of possible ways to decode string
 def helper data, k
 	# Exit code: When "" return 1
 	if k == 0
@@ -16,11 +17,14 @@ def helper data, k
 	
 	# Ex: result of "12345" equals "2345" + "345"(if two digit number less than or equal to 26)
 	result = helper(data, k - 1)
-
+	
 	# If two digit number less than or equal to 26
-	if k >= 2 and (data.slice(s, s + 2)).to_i <= 26
+	if k >= 2 and (data.slice(s, 2)).to_i <= 26
 		result += helper(data, k - 2)
-	end		
+	end	
+
+	# Return result
+	result	
 
 end	
 
@@ -33,3 +37,15 @@ def num_of_ways data
 
 	helper data, data.length
 end	
+
+p num_of_ways "123"  # 3
+p num_of_ways "023"  # 0
+p num_of_ways "1203" # 1
+p num_of_ways ""     # 1
+p num_of_ways "2723" # 2
+p num_of_ways "12"   # 2
+
+
+
+
+
