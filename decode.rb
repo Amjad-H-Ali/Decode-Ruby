6a4fc871c750eb84dd89543962e606fdd46ff218
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-09-23 22:14:41 -0500

    To store data and prevent redundant computation; Complete

--- decode.rb
+++ decode.rb
@@ -3,7 +3,7 @@ p "Decode!"
 # Function takes in string of digits and 
 # k representing current portion of string being evaluated
 # Returns number of possible ways to decode string
-def helper data, k
+def helper data, k, store
 	# Exit code: When "" return 1
 	if k == 0
 		return 1;	
@@ -15,13 +15,21 @@ def helper data, k
 		return 0;
 	end
 	
+	# If we found result before, return it and prevent rendundant work
+	if store[k]
+		return store[k]
+	end	
+
 	# Ex: result of "12345" equals "2345" + "345"(if two digit number less than or equal to 26)
-	result = helper(data, k - 1)
+	result = helper data, k - 1, store
 	
 	# If two digit number less than or equal to 26
 	if k >= 2 and (data.slice(s, 2)).to_i <= 26
-		result += helper(data, k - 2)
+		result += helper data, k - 2, store
 	end	
+
+	# Store result before returning it
+	store[k] = result
 
 	# Return result
 	result	
@@ -34,8 +42,10 @@ end
 # Function to take in digits in the form of a string as argument
 # and passes to helper function
 def num_of_ways data
+	# To store data and prevent redundant computation
+	store = {}
 
-	helper data, data.length
+	helper data, data.length, store
 end	
 
 p num_of_ways "123"  # 3
