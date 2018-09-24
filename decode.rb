d6be35f5cb76cd833e844eca8166c919536e7a42
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-09-23 19:26:02 -0500

    Function to take in digits in the form of a string as argument and passes to helper function; Function takes in string of digits and k representing current portion of string being evaluated

--- decode.rb
+++ decode.rb
@@ -1,1 +1,17 @@
 p "Decode!"
+
+# Function takes in string of digits and 
+# k representing current portion of string being evaluated
+def helper data, k
+
+end	
+
+
+
+
+# Function to take in digits in the form of a string as argument
+# and passes to helper function
+def num_of_ways data
+
+	helper data, data.length
+end	
